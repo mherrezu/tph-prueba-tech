923e19a730dc05dc695b58ba697de1eaddbe71c2
mherrezu <114182402+mherrezu@users.noreply.github.com>
2025-01-22 18:08:21 +0100

    amend some layout issues

--- src/components/PhiloSection.vue
+++ src/components/PhiloSection.vue
@@ -1,8 +1,9 @@
 <template>
-  <section class="philosophy-section">
+  <section id="philo" class="philosophy-section">
     <div class="philosophy-text">
       <h2 class="philosophy-title" v-html="title"></h2>
       <p class="philosophy-description">{{ description }}</p>
+      <RouterLink :to="`/${cta.url}`" class="cta-button">{{ cta.title }}</RouterLink>
     </div>
     <div class="philosophy-image">
       <img :src="image.url" :alt="image.alt" />
@@ -24,6 +25,10 @@ export default {
         "",
     },
     image: {
+      type: Object,
+      default: () => ({}),
+    },
+    cta: {
       type: Object,
       default: () => ({}),
     },
@@ -57,11 +62,12 @@ export default {
 .philosophy-description {
   font-size: 1.5rem;
   line-height: 1.8;
+  margin-bottom: 20px;
 }
 
 .philosophy-image {
   flex: 1;
-  max-width: 50%;
+  max-height: 100%;
   text-align: right;
 }
 
@@ -71,19 +77,49 @@ export default {
   border: 1px solid var(--color-dark-soft);
 }
 
+.cta-button {
+  padding: 10px 20px;
+  border: 1px solid var(--color-light);
+  border-radius: 20px;
+  font-size: 1.2rem;
+  color: var(--color-light);
+  cursor: pointer;
+  text-decoration: none;
+  transition: background-color 0.3s ease;
+}
+
+.cta-button:hover {
+  background-color: var(--color-light);
+  color: var(--color-dark);
+}
+
 @media (max-width: 768px) {
-  .philosophy-section {
-    flex-direction: column;
-    text-align: center;
+  .philosophy-title {
+    margin-bottom: 10px;
   }
 
-  .philosophy-text,
-  .philosophy-image {
+  .philosophy-description {
+    font-size: 1.2rem;
+  }
+}
+
+@media (max-width: 480px) {
+  .philosophy-section {
+    flex-direction: column;
+    padding: 40px 20px;
+  }
+
+  .philosophy-text {
     max-width: 100%;
   }
 
   .philosophy-image {
-    margin-top: 20px;
+    max-width: 100%;
+  }
+
+  .philosophy-image img {
+    width: 100%;
+    margin-top: 12px;
   }
 }
 </style>
