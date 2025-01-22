4288732be554cb17527e38b1940e82f1e5bbd6cf
mherrezu <114182402+mherrezu@users.noreply.github.com>
2025-01-22 20:37:21 +0100

    routing config

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/logup',
+      component: () => import('../views/LogupView.vue'),
+    },
+    {
+      path: '/profile',
+      component: () => import('../views/ProfileView.vue'),
+    },
+
   ],
 })
 
